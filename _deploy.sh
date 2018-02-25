1531adfe307eebf7fe45dd2cdaf6f210ff264582
w201rdada-portfolio-csinguva-dkumvcr0 <brooksambrose@berkeley.edu>
2018-02-25 03:39:55 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
- git config user.email "csinguva@berkeley.edu"
- git config user.name "csinguva"
+ git config user.email "brooksambrose@berkeley.edu"
+ git config user.name "Brooks Ambrose"
 
 # make sure you're on the master branch
 git checkout master
